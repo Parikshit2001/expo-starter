459583c2d19d317baf995132140bf35f0d0c6ad4
Parikshit <parikshitgupta2001@gmail.com>
2024-11-06 12:27:36 +0530

    Configure the fonts and colors in tailwind

--- app/index.tsx
+++ app/index.tsx
@@ -5,7 +5,7 @@ import { Link } from "expo-router";
 const index = () => {
   return (
     <View className="flex-1 justify-center items-center">
-      <Text>index</Text>
+      <Text className="font-pblack">index</Text>
       <Link href="/profile" className="text-blue-500">
         Go to Profile
       </Link>
